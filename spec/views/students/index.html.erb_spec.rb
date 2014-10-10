5e5a8a008b3f594a871b1c81292be95f6f5fd20a
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-10 15:12:07 -0400

    scaffolded students

---
+++ spec/views/students/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "students/index", :type => :view do
+  before(:each) do
+    assign(:students, [
+      Student.create!(
+        :name => "Name",
+        :age => 1
+      ),
+      Student.create!(
+        :name => "Name",
+        :age => 1
+      )
+    ])
+  end
+
+  it "renders a list of students" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+  end
+end
